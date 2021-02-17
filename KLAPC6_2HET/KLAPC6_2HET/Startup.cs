6d1f231a9d693410f6ab555a7172cf97e765e4dc
corvinus-klapc6 <daniel.barabas@stud.uni-corvinus.hu>
2021-02-17 21:51:13 +0100

    Első hét behozása

--- KLAPC6_2HET/KLAPC6_2HET/Startup.cs
+++ KLAPC6_2HET/KLAPC6_2HET/Startup.cs
@@ -26,15 +26,20 @@ namespace KLAPC6_2HET
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles(); //A sorrend fontos!
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
